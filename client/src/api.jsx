4e8b12dec7cfdab713159f35d67097da52d48fa2
ullas9080 <ullas9080@gmail.com>
2025-11-30 21:20:55 +0530

    updated url

--- client/src/api.jsx
+++ client/src/api.jsx
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api=axios.create({
-    baseURL:"http://localhost:3000",
+    baseURL:"https://monetiz-iq-xslp.vercel.app/",
     timeout:5000,
     headers:{"Content-Type":"application/json"}
 })
--- client/src/features/apiSlice.js
+++ client/src/features/apiSlice.js
@@ -3,7 +3,7 @@ import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 export const apiSlice = createApi({
   reducerPath: "api",
   baseQuery: fetchBaseQuery({
-    baseUrl: "http://localhost:3000/",
+    baseUrl: "https://monetiz-iq-xslp.vercel.app/",
     credentials: "include",
   }),
   tagTypes: ["Channel", "Auth", "Video"],
