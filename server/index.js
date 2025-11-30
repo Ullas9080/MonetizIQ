6fd1de0cdc232ffd53c07904f5e170db09220da2
ullas9080 <ullas9080@gmail.com>
2025-12-01 02:04:20 +0530

    i am back

--- server/index.js
+++ server/index.js
@@ -19,18 +19,20 @@ app.use(
 );
 
 connectDB();
-app.get("/", (req, res) => {
+
+app.get("/ping", (req, res) => {
   res.send("Server is alive");
 });
+
 app.use("/oauth2callback", authRoute);
 app.use("/api/channel", channelRoute);
 app.use("/gemini", geminiRoute);
+
 
 if (process.env.NODE_ENV !== "production") {
   const PORT = process.env.PORT || 5000;
   app.listen(PORT, () => console.log(`Server running locally on port ${PORT}`));
 }
 
-export default function handler(req, res) {
-  app(req, res);
-}
+
+export default app;
