9dc93290f51d4b245910ed3b95ba94bd9333a7be
ullas9080 <ullas9080@gmail.com>
2025-11-30 22:07:28 +0530

    updated index

--- server/index.js
+++ server/index.js
@@ -3,29 +3,34 @@ import dotenv from "dotenv";
 import cors from "cors";
 import connectDB from "./configs/db.js";
 import authRoute from "./routes/authRoute.js";
-import channelRoute from "./routes/channelRoutes.js"
-import geminiRoute from "./routes/geminiRoute.js"
+import channelRoute from "./routes/channelRoutes.js";
+import geminiRoute from "./routes/geminiRoute.js";
+
 dotenv.config();
 
 const app = express();
-const PORT ="https://monetiz-iq-fh2u.vercel.app/" ;
 
 app.use(express.json());
 app.use(
   cors({
-    origin: process.env.FRONTEND_URL || "https://monetiz-iq.vercel.app/",
+    origin: process.env.FRONTEND_URL || "https://monetiz-iq.vercel.app",
     credentials: true,
   })
 );
 
-connectDB();            
+connectDB();
 
 
 app.use("/oauth2callback", authRoute);
-app.use("/api/channel",channelRoute)
-app.use("/gemini",geminiRoute)
+app.use("/api/channel", channelRoute);
+app.use("/gemini", geminiRoute);
 
 
-app.listen(PORT, () => {
-  console.log(`Server Started at ${PORT}`);
-});
+if (process.env.NODE_ENV !== "production") {
+  const PORT = process.env.PORT || 5000;
+  app.listen(PORT, () => {
+    console.log(`Server running locally on port ${PORT}`);
+  });
+}
+
+export default app;
