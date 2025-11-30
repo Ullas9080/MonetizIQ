0f897eb62323ec09b03ab1599af088d47a50f0f7
ullas9080 <ullas9080@gmail.com>
2025-11-30 22:13:26 +0530

    updated index

--- server/index.js
+++ server/index.js
@@ -26,11 +26,10 @@ app.use("/api/channel", channelRoute);
 app.use("/gemini", geminiRoute);
 
 
-if (process.env.NODE_ENV !== "production") {
-  const PORT = process.env.PORT || 5000;
-  app.listen(PORT, () => {
-    console.log(`Server running locally on port ${PORT}`);
-  });
-}
+// if (process.env.NODE_ENV !== "production") {
+//   const PORT = process.env.PORT || 5000;
+//   app.listen(PORT, () => {
+//     console.log(`Server running locally on port ${PORT}`);
+//   });
+// }
 
-export default app;
