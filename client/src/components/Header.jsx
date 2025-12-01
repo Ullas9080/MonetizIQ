2112886bcc5a6bbb59d55c6dcdb44d26e7427845
ullas9080 <ullas9080@gmail.com>
2025-12-02 00:30:41 +0530

    completed finally

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -27,7 +27,7 @@ const Header = () => {
     localStorage.clear();
     sessionStorage.clear();
 
- window.location.href = "http://localhost:3000/oauth2callback/logout";
+ window.location.href = "https://monetiz-iq-odqj.vercel.app/oauth2callback/logout";
   };
 
   return (
--- client/src/components/Login.jsx
+++ client/src/components/Login.jsx
@@ -2,7 +2,7 @@ import { TypeAnimation } from "react-type-animation";
 
 const Login = () => {
   const handleLogin = () => {
-    window.location.href = `http://localhost:3000/oauth2callback/google`;
+    window.location.href = `https://monetiz-iq-odqj.vercel.app/oauth2callback/google`;
   };
 
   return (
